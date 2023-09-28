c647e55048e966f7c5d418022de1550ee86c780d
Gnom <gnomyeah@gmail.com>
2023-09-28 22:38:26 +0300

    add cather_404

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'falsdaw123mfsnja1l21dsadf1'
+ 
 
-@app.route("/")
+
+@app.route("/", methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        print(request.form)
+
     return render_template("index.html")
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template("error_404.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
