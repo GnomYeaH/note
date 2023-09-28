da861b1071775372e8a4fbf2a73943f78c148abd
painevi <marat.shpagin2005@mail.ru>
2023-09-28 21:36:03 +0300

    huipizda

---
+++ app.js
@@ -0,0 +1,15 @@
+const inputElement = document.getElementById("title");
+const createBtn = document.getElementById("create");
+const listElement = document.getElementById("list");
+createBtn.addEventListener('click', function() {
+    listElement.innerHTML = `<li
+    class="list-group-item d-flex justify-content-between align-items-center"
+  >
+    <span>${inputElement.value}</span>
+    <span>
+      <span class="btn btn-small btn-success">&check;</span>
+      <span class="btn btn-small btn-danger">&times;</span>
+    </span>
+  </li>`
+})
+
