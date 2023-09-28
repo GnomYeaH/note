b49df9f47a63e8c721052e98398f39d92e56577e
painevi <marat.shpagin2005@mail.ru>
2023-09-28 21:55:58 +0300

    normhtml

--- app.js
+++ app.js
@@ -12,4 +12,14 @@ createBtn.addEventListener('click', function() {
     </span>
   </li>`
 })
+listElement.insertAdjacentHTML('beforeend', 
+  `<li
+  class="list-group-item d-flex justify-content-between align-items-center"
+  >
+  <span>${inputElement.value}</span>
+  <span>
+    <span class="btn btn-small btn-success">&check;</span>
+    <span class="btn btn-small btn-danger">&times;</span>
+  </span>
+</li>` )
 
