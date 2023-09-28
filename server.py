b96cb2330978fe88b8aebdbcfc85044f4e36fc0b
Gnom <gnomyeah@gmail.com>
2023-09-28 22:01:41 +0300

    add templates

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Pro Flask")
+    return render_template("index.html")
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
