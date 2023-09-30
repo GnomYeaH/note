120f65a89efac6cb01d17b21488600227e058440
painevi <marat.shpagin2005@mail.ru>
2023-09-30 15:18:56 +0300

    papochkastaticjs

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,22 @@
 const inputElement = document.getElementById("title");
 const createBtn = document.getElementById("create");
 const listElement = document.getElementById("list");
+
 createBtn.addEventListener('click', function() {
-    listElement.innerHTML = `<li
+  if(inputElement.value.length === 0) {
+  return;
+  }
+  listElement.insertAdjacentHTML(
+    'beforeend', 
+  `<li
     class="list-group-item d-flex justify-content-between align-items-center"
-  >
+    >
     <span>${inputElement.value}</span>
     <span>
       <span class="btn btn-small btn-success">&check;</span>
       <span class="btn btn-small btn-danger">&times;</span>
     </span>
   </li>`
-})
-listElement.insertAdjacentHTML('beforeend', 
-  `<li
-  class="list-group-item d-flex justify-content-between align-items-center"
-  >
-  <span>${inputElement.value}</span>
-  <span>
-    <span class="btn btn-small btn-success">&check;</span>
-    <span class="btn btn-small btn-danger">&times;</span>
-  </span>
-</li>` )
-
+) 
+}
+) 
